356f0f09d0b768a0d69e294a6f027c7b47158b9f
Flores, Omar (Cognizant) <lenin.omar@hotmail.com>
2019-05-06 15:18:50 -0500

    Added some 2D projects

---
+++ 2D/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max, min, guess;
+
+	// Use this for initialization
+	void Start () {
+        StartGame();
+	}
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        Debug.Log("*** Welcome to Number Wizard ***");
+        Debug.Log("Pick a number, don't tell me what it is");
+        Debug.Log("Highest number you can pick is: " + max);
+        Debug.Log("Lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Up arrow = higher, Down arrow = lower, Enter = Correct!");
+        max++;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            min = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            max = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.Return)) {
+            Debug.Log("I won!");
+            StartGame();
+        }
+	}
+
+    void NextGuess() {
+        guess = (max + min) / 2;
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+    }
+}
---
+++ 2D/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+	// Use this for initialization
+	void Start () {
+        NextGuess();
+	}
+
+    public void OnPressHigher() {
+        if (guess < 1000) {
+            min = guess + 1;
+            NextGuess();
+        }
+    }
+
+    public void OnPressLower() {
+        if (guess > 1) {
+            max = guess - 1;
+            NextGuess();
+        }
+    }
+
+    void NextGuess() {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+
+}
